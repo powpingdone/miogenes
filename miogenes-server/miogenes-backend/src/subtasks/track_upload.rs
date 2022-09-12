bbfe3f2e4cdd65dd78a56e1acfb5524eb54738d2
Aidan Case <aidanzcase@gmail.com>
2022-09-12 15:27:27 -0500

    resize img for smaller blurhash length

--- miogenes-server/miogenes-backend/src/subtasks/track_upload.rs
+++ miogenes-server/miogenes-backend/src/subtasks/track_upload.rs
@@ -172,10 +172,12 @@ fn get_metadata(fname: impl AsRef<Path> + Clone) -> Result<Metadata, anyhow::Err
                     mdata.imghash = Some(hash(&img));
                     trace!("{fname}: imghash: {:?}", mdata.imghash);
 
+                    const BHASH_W: u32 = 128;
+                    const BHASH_H: u32 = 128;
                     let dropped = image::load_from_memory(&img)?;
-                    let (w, h) = dropped.dimensions();
+                    let shrunk_img = dropped.resize_exact(BHASH_W, BHASH_H, image::imageops::FilterType::Nearest);
                     mdata.blurhash = Some(
-                        blurhash::encode(6, 6, w, h, &dropped.to_rgba8().into_vec())
+                        blurhash::encode(6, 6, BHASH_W, BHASH_H, &shrunk_img.to_rgba8().into_vec())
                             .as_bytes()
                             .to_vec(),
                     );
