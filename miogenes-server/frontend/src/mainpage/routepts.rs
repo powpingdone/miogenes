7a89dbdceac0b773b8af75f75d1a67cbb282f83d
Aidan Case <aidanzcase@gmail.com>
2023-02-27 02:29:37 -0600

    Try a different approach to uploading files

--- miogenes-server/frontend/src/mainpage/routepts.rs
+++ miogenes-server/frontend/src/mainpage/routepts.rs
@@ -7,12 +7,17 @@ use uuid::*;
 use gloo_net::http::{
     Request,
 };
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{
+    JsCast,
+    prelude::*,
+};
 use web_sys::{
-    HtmlInputElement,
     Blob,
+    FileReader,
+    HtmlInputElement,
 };
 use wasm_bindgen_futures::*;
+use std::sync::Arc;
 
 #[inline_props]
 #[allow(non_snake_case)]
@@ -20,9 +25,8 @@ pub fn MainPage(cx: Scope, token: UseRef<Option<Uuid>>) -> Element {
     cx.render(rsx!{
         Router {
             Route {
-                to: "/" HomePage {
-                    token: token.clone()
-                }
+                to: "/",
+                HomePage { token: token.clone() }
             }
         }
     })
@@ -39,49 +43,53 @@ pub fn HomePage(cx: Scope, token: UseRef<Option<Uuid>>) -> Element {
     });
     cx.render(rsx!{
         div {
-            form {
-                input {
-                    r#type: "file",
-                    id: "inp",
-                    // this does mark this as a multiple file upload
-                    multiple: "false",
-                }
-                button {
-                    onclick: move | _ | {
-                        log::trace!("onclick");
-                        let files =
-                            web_sys::window()
-                                .unwrap()
-                                .document()
-                                .unwrap()
-                                .get_element_by_id("inp")
-                                .unwrap()
-                                .dyn_ref::<HtmlInputElement>()
-                                .unwrap()
-                                .files()
-                                .unwrap();
-                        let send_files = vec![];
-                        for pos in 0 .. files.length() {
-                            let file = files.item(pos).unwrap();
-                            send_files.push((file.name(), {
-                                let stream = AsRef::<Blob>::as_ref(&file).stream();
-                                stream.tee()
-                            }));
-                        }
-                        log::trace!("entering future.");
-                        spawn_local({
-                            async move {
-                                log::trace!("entered future.");
-                                log::trace!(
-                                    "{:?}",
-                                    Request::put(&format!("/track/tu")).body(send_files).send().await
-                                );
+            input {
+                r#type: "file",
+                id: "inp",
+                // this does mark this as a multiple file upload
+                multiple: "false",
+            }
+            button {
+                onclick: move | evt | {
+                    log::trace!("onclick");
+                    evt.stop_propagation();
+                    let files =
+                        web_sys::window()
+                            .unwrap()
+                            .document()
+                            .unwrap()
+                            .get_element_by_id("inp")
+                            .unwrap()
+                            .dyn_ref::<HtmlInputElement>()
+                            .unwrap()
+                            .files()
+                            .unwrap();
+                    for pos in 0 .. files.length() {
+                        let file = files.item(pos).unwrap();
+                        let fname = file.name();
+                        let blob: Blob = file.into();
+                        let reader = Arc::new(FileReader::new().unwrap());
+                        let cl = Closure::once_into_js({
+                            let reader = reader.clone();
+                            move || {
+                                spawn_local({
+                                    async move {
+                                        let req =
+                                            Request::put(&format!("/track/tu?fname={fname}"))
+                                                .body(reader.result().unwrap())
+                                                .send()
+                                                .await;
+                                        log::trace!("{req:?}");
+                                    }
+                                });
                             }
                         });
-                        fut.restart();
-                    },
-                    "Send over."
-                }
+                        reader.set_onload(Some(cl.as_ref().unchecked_ref()));
+                        reader.read_as_binary_string(&blob).unwrap();
+                    }
+                    fut.restart();
+                },
+                "Send over."
             }
             div {
                 {
