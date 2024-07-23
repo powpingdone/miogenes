11b7c95838827fa521c04ecd85739ee639cf79f9
Aidan <aidanzcase@gmail.com>
2024-07-23 18:52:27 -0500

    gst

--- frontend/rust/src/player.rs
+++ frontend/rust/src/player.rs
@@ -1,9 +1,29 @@
+use gstreamer::{self as gst, prelude::GstBinExtManual};
+use std::collections::HashMap;
+use uuid::Uuid;
+
 // frontend interaction with audio player
-pub struct AudioPlayer {}
+pub struct AudioPlayer {
+    order: Vec<Uuid>,
+    now_playing: usize,
+    decoder_pipelines: HashMap<Uuid, gst::Pipeline>,
+    audio_sink: gst::Pipeline,
+}
 
 impl AudioPlayer {
-    pub fn new() -> Result<Self, gstreamer::glib::Error> {
-        gstreamer::init()?;
-        Ok(Self {})
+    pub fn new() -> Result<Self, anyhow::Error> {
+        gst::init()?;
+        let audio_sink = gst::Pipeline::builder().name("audio_sink").build();
+        let conv = gst::ElementFactory::make("audioconvert").build()?;
+        let samp = gst::ElementFactory::make("audioresample").build()?;
+        let sink = gst::ElementFactory::make("autoaudiosink").build()?;
+        audio_sink.add_many([&conv, &samp, &sink])?;
+        gst::Element::link_many([&conv, &samp, &sink])?;
+        Ok(Self {
+            order: vec![],
+            now_playing: 0,
+            decoder_pipelines: HashMap::new(),
+            audio_sink,
+        })
     }
 }
