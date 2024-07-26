8af59f0d6f3ace03280f68812bc451036170a1ee
Aidan <aidanzcase@gmail.com>
2024-07-25 21:39:24 -0500

    more gst

--- frontend/rust/src/player.rs
+++ frontend/rust/src/player.rs
@@ -1,17 +1,20 @@
 use gstreamer::{self as gst, prelude::GstBinExtManual};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use uuid::Uuid;
+
+pub struct NoQueueLeft;
 
 // frontend interaction with audio player
 pub struct AudioPlayer {
-    order: Vec<Uuid>,
+    order: VecDeque<Uuid>,
     now_playing: usize,
     decoder_pipelines: HashMap<Uuid, gst::Pipeline>,
     audio_sink: gst::Pipeline,
+    limit: usize,
 }
 
 impl AudioPlayer {
-    pub fn new() -> Result<Self, anyhow::Error> {
+    pub fn new(limit: usize) -> Result<Self, anyhow::Error> {
         gst::init()?;
         let audio_sink = gst::Pipeline::builder().name("audio_sink").build();
         let conv = gst::ElementFactory::make("audioconvert").build()?;
@@ -20,10 +23,29 @@ impl AudioPlayer {
         audio_sink.add_many([&conv, &samp, &sink])?;
         gst::Element::link_many([&conv, &samp, &sink])?;
         Ok(Self {
-            order: vec![],
+            order: VecDeque::with_capacity(limit), 
             now_playing: 0,
             decoder_pipelines: HashMap::new(),
             audio_sink,
+            limit
         })
+    }
+
+    pub fn queue(&mut self, id: Uuid) -> Result<Option<Uuid>, NoQueueLeft> {
+        if self.limit >= self.order.len() {
+            if self.now_playing == 0 {
+                return Err(NoQueueLeft);
+            }
+            let new_id = self.order.pop_front().unwrap();
+            drop(self.decoder_pipelines.remove(&new_id).unwrap());
+            self.add_pipeline(id);
+            return Ok(Some(new_id));
+        }
+        self.add_pipeline(id);
+        return Ok(None);
+    }
+
+    fn add_pipeline(&mut self, id: Uuid) {
+        todo!()
     }
 }
