54c124772cdeda281722bdbf73826564c5b66d2d
Aidan <aidanzcase@gmail.com>
2024-07-27 20:41:59 -0500

    gst moment

--- frontend/rust/src/player.rs
+++ frontend/rust/src/player.rs
@@ -9,43 +9,45 @@ pub struct AudioPlayer {
     order: VecDeque<Uuid>,
     now_playing: usize,
     decoder_pipelines: HashMap<Uuid, gst::Pipeline>,
-    audio_sink: gst::Pipeline,
     limit: usize,
 }
 
 impl AudioPlayer {
     pub fn new(limit: usize) -> Result<Self, anyhow::Error> {
         gst::init()?;
-        let audio_sink = gst::Pipeline::builder().name("audio_sink").build();
-        let conv = gst::ElementFactory::make("audioconvert").build()?;
-        let samp = gst::ElementFactory::make("audioresample").build()?;
-        let sink = gst::ElementFactory::make("autoaudiosink").build()?;
-        audio_sink.add_many([&conv, &samp, &sink])?;
-        gst::Element::link_many([&conv, &samp, &sink])?;
         Ok(Self {
-            order: VecDeque::with_capacity(limit), 
+            order: VecDeque::with_capacity(limit),
             now_playing: 0,
             decoder_pipelines: HashMap::new(),
-            audio_sink,
-            limit
+            limit,
         })
     }
 
     pub fn queue(&mut self, id: Uuid) -> Result<Option<Uuid>, NoQueueLeft> {
-        if self.limit >= self.order.len() {
+        let new_id = if self.limit >= self.order.len() {
             if self.now_playing == 0 {
                 return Err(NoQueueLeft);
             }
-            let new_id = self.order.pop_front().unwrap();
-            drop(self.decoder_pipelines.remove(&new_id).unwrap());
-            self.add_pipeline(id);
-            return Ok(Some(new_id));
-        }
+            self.order.pop_front()
+        } else {
+            None
+        };
+        new_id.and_then(|x| self.decoder_pipelines.remove(&x));
         self.add_pipeline(id);
-        return Ok(None);
+        return Ok(new_id);
     }
 
     fn add_pipeline(&mut self, id: Uuid) {
         todo!()
     }
+}
+
+fn make_sink() -> anyhow::Result<gst::Pipeline> {
+    let audio_sink = gst::Pipeline::builder().name("audio_sink").build();
+    let conv = gst::ElementFactory::make("audioconvert").build()?;
+    let samp = gst::ElementFactory::make("audioresample").build()?;
+    let sink = gst::ElementFactory::make("autoaudiosink").build()?;
+    audio_sink.add_many([&conv, &samp, &sink])?;
+    gst::Element::link_many([&conv, &samp, &sink])?;
+    Ok(audio_sink)
 }
